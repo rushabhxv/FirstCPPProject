a2cfd196dcc3709db2bfc06836444693a25ea596
Rushabh Patil <102453840+rushabhxv@users.noreply.github.com>
2022-07-09 08:15:33 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    int num2;
+
+    cout << "Enter first number: ";
+    cin >> num;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+    cout << "the addition of the two numbers provided by you:\n" << num + num2;
+    return 0;
+}
